708ce92c87b35b179ef771228aa43e2f6e4b5ce8
rne1223 <rne1223@gmail.com>
2023-04-20 13:08:12 -0700

    Init upload of project

---
+++ myjobot-web/src/pages/index.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+export default function Home() {
+  // const [apiKey, setApiKey] = useState('');
+
+  return (
+  <div className="flex flex-col h-screen">
+    <nav className="shadow p-4 flex-row justify-between"> 
+      <div className="text-4xl"> Jobot </div>
+      {/* <div>
+        <input type="password" className="border" placeholder="Paste Api key here"/>
+      </div> */}
+    </nav>
+  </div>
+  )
+}
