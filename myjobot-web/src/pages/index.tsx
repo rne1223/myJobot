9cc411748f3b8b0cdb66da37b71870745d3a1095
rne1223 <rne1223@gmail.com>
2023-05-01 14:54:29 -0700

    Added a head to index and modified the taillwind config

--- myjobot-web/src/pages/index.tsx
+++ myjobot-web/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import ReactMarkdown from 'react-markdown';
+import Head from "next/head";
 
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const SYSTEM_MESSAGE = "You are YourJobot, a helpful AI developed by you and powered by state-of-the-art machine learning models."
@@ -45,52 +47,58 @@ export default function Home() {
   };
 
   return (
-  <div className="flex flex-col h-screen">
-    {/* Navigation */}
-    <nav className="shadow px-4 py-2 flex flex-row justify-between items-center"> 
-      <div className="text-xl font-bold"> Jobot </div>
+    <>
+      <Head>
+        <title>MyJobot - A chatgpt bot </title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navigation */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center"> 
+          <div className="text-xl font-bold"> Jobot </div>
 
-      {/* Api input */}
-      <div>
-        <input 
-        type="password" 
-        className="border p-1 rounded" 
-        onChange={(e) => {setApiKey(e.currentTarget.value)}}
-        value={apiKey}
-        placeholder="Paste Api key here"/>
-      </div>
-    </nav>
-
-    {/* Message History */}
-    <div className="flex-1">
-      <div className="w-full max-w-screen-md mx-auto px-4">
-        {messages.filter(message => message.role !== "system").map((message, idx) => (
-          <div key={idx} className="mt-3">
-            <div className="font-bold"> 
-              {message.role === "user" ? "You" : "Jobot"} 
-            </div>
-            <div className="text-lg"> {message.content} </div>
+          {/* Api input */}
+          <div>
+            <input 
+            type="password" 
+            className="border p-1 rounded" 
+            onChange={(e) => {setApiKey(e.currentTarget.value)}}
+            value={apiKey}
+            placeholder="Paste Api key here"/>
           </div>
-        ))}
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll">
+          <div className="w-full max-w-screen-md mx-auto px-4">
+            {messages.filter(message => message.role !== "system").map((message, idx) => (
+              <div key={idx} className="my-3">
+                <div className="font-bold"> 
+                  {message.role === "user" ? "You" : "Jobot"} 
+                </div>
+                <div className="text-lg prose"> 
+                  <ReactMarkdown>{message.content}</ReactMarkdown>
+                </div>
+              </div>
+            ))}
+          </div> </div>
+
+        {/* Message Input */}
+        <div>
+          <div className="w-full flex max-w-screen-md mx-auto px-4 pb-2">
+            <textarea value={userMessage} 
+            onChange={e => {setUserMessage(e.target.value); } }
+            className="border text-lg rounded-md p-1 flex-1" 
+            rows={1}/>
+
+            <button 
+            onClick={sendRequest}
+            className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2">
+              Send
+            </button>
+          </div>
+        </div>
+
       </div>
-    </div>
-
-    {/* Message Input */}
-    <div>
-      <div className="w-full flex max-w-screen-md mx-auto px-4 pb-2">
-        <textarea value={userMessage} 
-        onChange={e => {setUserMessage(e.target.value); } }
-        className="border text-lg rounded-md p-1 flex-1" 
-        rows={1}/>
-
-        <button 
-        onClick={sendRequest}
-        className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2">
-          Send
-        </button>
-      </div>
-    </div>
-
-  </div>
+    </>
   )
 }
