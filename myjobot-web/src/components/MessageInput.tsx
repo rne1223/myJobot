0b23ddc08f2d034d59d7ba95a87a10fe4b1c1a7f
rne1223 <rne1223@gmail.com>
2023-06-12 16:20:49 -0700

    Renamed userInput for MessageInput. Using react-textarea for better user experience.

---
+++ myjobot-web/src/components/MessageInput.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect, useState } from "react";
+import cn from "classnames";
+import { AiOutlineSend } from "react-icons/ai";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import TextArea from "react-textarea-autosize";
+
+type MessageInputProps = {
+  sending: boolean,
+  sendMessages: Function,
+  placeholder?: string,
+} 
+
+
+const MessageInput = ({ sending, sendMessages, placeholder = "Start typing here...",
+}: MessageInputProps) => {
+
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
+  const [prompt, setPrompt] = useState("");
+  const handleSendClick = () => {
+    console.log(`MessageInput: ${prompt}`);
+
+    sendMessages([{role:"user", content:prompt}])
+    .then((success:any) => !success && setPrompt(prompt));
+
+    setPrompt("");
+  };
+
+
+  const Icon = sending ? HiOutlineDotsHorizontal : AiOutlineSend;
+
+  useEffect(() => {
+    !sending && inputRef?.current?.focus();
+  }, [sending]);
+
+  return (
+    <div className="mx-auto w-full max-w-4xl p-2">
+      <div className="flex items-end rounded-md border p-4 pr-2 dark:border-gray-400">
+        <TextArea
+          ref={inputRef}
+          minRows={1}
+          maxRows={10}
+          autoFocus
+          disabled={sending}
+          placeholder={sending ? "Wait for my response.." : placeholder}
+          className={cn(
+            sending && "bg-gray-100 text-gray-400",
+            "w-full flex-1 resize-none self-center bg-transparent leading-tight focus:outline-none dark:border-gray-500"
+          )}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && !event.shiftKey) {
+              event.preventDefault();
+              handleSendClick();
+            }
+          }}
+        />
+        <button
+          className="rounded-full p-4 bg-blue-500 hover:bg-blue-600 text-white text-lg"
+          title="Send"
+          onClick={handleSendClick}
+        >
+          <Icon />
+          {/* <Icon variant="primary" disabled={!prompt || sending} /> */}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MessageInput;
